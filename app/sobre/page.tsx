5cf159d420abd9ef5b64a62a212594d870168880
vitoriaacaroline <134931502+vitoriaacaroline@users.noreply.github.com>
2024-03-01 11:42:38 -0300

    reset css e link

---
+++ app/sobre/page.tsx
@@ -0,0 +1,11 @@
+import Link from "next/link";
+
+export default function Sobre () {
+    return(
+       <>
+          <h1>Sobre</h1>
+          <p> Tela sobre meu projeto</p> 
+          <Link href={"/"}>Home</Link>
+       </>
+    );
+}
